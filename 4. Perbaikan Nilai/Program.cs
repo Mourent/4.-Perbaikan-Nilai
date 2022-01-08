dba4650caf49d3e41b3a12a7962d0a16183e8561
Jefferson Mourent <jmourent@ciputra.student.ac.id>
2022-01-08 10:11:33 +0700

    Add project files.

---
+++ 4. Perbaikan Nilai/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _4._Perbaikan_Nilai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=0; i<4; i++)
+            {
+                Random random = new Random();
+                int test = random.Next(0, 10);
+                Console.WriteLine("Angka" + (i + 1) + " = " + test);
+            }
+        }
+    }
+}
